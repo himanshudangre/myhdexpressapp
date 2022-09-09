8b186fd20e159cda4afb1ba4f15a8e8570dae6e1
Himanshu Dangre <himanshudangre99@gmail.com>
2022-09-09 12:04:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app=express()
+var cors = require('cors')
+const port = process.env.PORT || 3001;
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.send("Hello world")
+})
+app.listen(port,function(req,res){
+    console.log("server started")
+})
